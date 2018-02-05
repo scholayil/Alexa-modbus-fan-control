b2df485d284124c9b9b0dcd91e22f84c2e67596e
Sameer Cholayil <scholayil@gmail.com>
2018-02-05 10:45:26 -0600

    test

---
+++ ifan_alexa.py
@@ -0,0 +1,102 @@
+
+import logging
+
+from random import randint
+
+from flask import Flask, render_template
+
+from flask_ask import Ask, statement, question, session
+
+import ifan_interface
+
+
+app = Flask(__name__)
+
+ask = Ask(app, "/")
+
+#logging.getLogger("flask_ask").setLevel(logging.INFO)
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+
+def process_response(resp):
+    if resp == 0:
+        msg = render_template('set')
+    else:
+        msg = render_template('noresponse')
+    return question(msg)
+
+
+@ask.launch
+def launch():
+    welcome_msg = render_template('welcome')
+    return question(welcome_msg)
+
+
+@ask.intent("AMAZON.CancelIntent")
+def ends():
+    end_msg = render_template('cancel')
+    return statement(end_msg)
+
+
+@ask.intent("INTENT_IFAN_GetSiteID")
+def get_site_id_from_modbus():
+    site_id = ifan_interface.get_site_id()
+    speech_output = "Your fan site id is " + str(site_id)
+    return question(speech_output)
+
+
+@ask.intent("INTENT_IFAN_Start")
+def set_state_start_on_modbus():
+    response = ifan_interface.set_start_stop(1)
+    return process_response(response)
+
+@ask.intent("INTENT_IFAN_Stop")
+def set_state_stop_on_modbus():
+    response = ifan_interface.set_start_stop(0)
+    return process_response(response)
+
+
+        
+@ask.intent("INTENT_IFAN_PushAirDown")
+def set_direction_on_modbus():
+    response = ifan_interface.set_direction(1)
+    return process_response(response)
+
+
+@ask.intent("INTENT_IFAN_PushAirUp")
+def set_direction_on_modbus():
+    response = ifan_interface.set_direction(0)
+    return process_response(response)
+
+
+@ask.intent("INTENT_IFAN_MaxSpeed")
+def set_max_speed_on_modbus():
+    response = ifan_interface.set_speed(10)
+    return process_response(response)
+
+
+@ask.intent("INTENT_IFAN_MinSpeed")
+
+
+
+@ask.intent("INTENT_IFAN_SpeedUp")
+@ask.intent("INTENT_IFAN_SpeedDown")
+
+@ask.intent("INTENT_IFAN_Status")
+
+def dave():
+    msg =  render_template('mainframe')
+    return question(msg)
+
+
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
+
+
+
+
+    
+
---
+++ ifan_interface.py
@@ -0,0 +1,45 @@
+
+from pymodbus.client.sync import ModbusTcpClient
+
+#Typical Modbus IP stuff; later configure from app
+KstrFanIP='192.168.2.15'
+KintFanPort=502
+KmbUnitID=1
+
+#register holds site name on define PCB (w/ Macro)
+KpcbSiteIDReg=6125#16658
+KpcbRunReg=5122
+KpcbDirReg=5121
+KpcbSpdReg=5120
+
+
+def get_site_id():
+    client = ModbusTcpClient(KstrFanIP, KintFanPort)
+    result = client.read_holding_registers(KpcbSiteIDReg,1,unit=KmbUnitID)
+    client.close()
+    return(result.registers[0])
+
+
+def set_start_stop(start1):
+    client = ModbusTcpClient(KstrFanIP, KintFanPort)
+    result = client.write_register(KpcbRunReg, start1, unit=KmbUnitID)
+    print("Set start:\n")
+    print(result)
+    return(0)
+
+
+def set_direction(clock1):
+    client = ModbusTcpClient(KstrFanIP, KintFanPort)
+    result = client.write_register(KpcbDirReg, clock1, unit=KmbUnitID)
+    print('Set Dir:\n')
+    print(result)
+    return(0)
+
+def set_speed(to):
+    client = ModbusTcpClient(KstrFanIP, KintFanPort)
+    result = client.write_register(KpcbSpdReg, to, unit=KmbUnitID)
+    print('Set Speed:\n')
+    print(result)
+    return(0)
+
+
